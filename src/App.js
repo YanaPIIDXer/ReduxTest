7e57ea013e1b3cc3f644be8a102497c0518bfd2a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-11-03 00:21:26 +0900

    Component派生クラスに変更

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-function App() {
-  return (
-    <div class="container">
-      <div class="text-center">
-        <h1 class="mt-3">Redux Test</h1>
-      </div>
-    </div>
-  );
-}
+import { Component } from 'react';
 
-export default App;
+export class App extends Component {
+  render() {
+    return (
+      <div class="container">
+        <div class="text-center">
+          <h1 class="mt-3">Redux Test</h1>
+        </div>
+      </div>
+    );
+  }
+};
