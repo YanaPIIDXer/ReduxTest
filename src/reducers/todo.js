64d4caad996bf575149f3d2ea474cb9ee790aa53
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-11-03 00:03:00 +0900

    Reducerを定義

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,3 +1,20 @@
 const initialState = {
     todoes: []
 }
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD_TODO":
+
+            return Object.assign([], state, {
+                todoes: [
+                    ...state.todoes,
+                    {
+                        text: action.text,
+                        done: false
+                    }
+                ]
+            });
+    }
+    return state;
+};
