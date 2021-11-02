6bca1b79229d83e238a2933cc59430831cab2de9
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-11-03 00:15:50 +0900

    Warning解消

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -2,7 +2,7 @@ const initialState = {
     todoes: []
 }
 
-export default (state = initialState, action) => {
+const todo = (state = initialState, action) => {
     switch (action.type) {
         case "ADD_TODO":
 
@@ -15,6 +15,13 @@ export default (state = initialState, action) => {
                     }
                 ]
             });
+
+        default:
+            // ここでreturn stateしてもいいと思うけど、
+            // 個人的には戻り値を返す関数の最終行がreturn文じゃないのは気持ちが悪いので
+            break;
     }
     return state;
 };
+
+export default todo;
