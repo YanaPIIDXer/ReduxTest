f6e3db370db4ff026b8d3d6bab9a64f427f28fc9
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-11-03 00:53:34 +0900

    入力された内容を表示するようにした

--- src/App.js
+++ src/App.js
@@ -2,11 +2,52 @@ import { Component } from 'react';
 import { connect } from 'react-redux';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.elements = null;
+    this.onSubmitTodo = this.onSubmitTodo.bind(this);   // メソッド内でthisを使えるようにするためのおまじない
+  }
+  
+  onSubmitTodo(event) {
+    event.preventDefault();
+
+    const text = this.elements["todo"].value;
+    if (text === "") {
+      alert("TODOを入力してください。");
+      return;
+    }
+    alert(text);
+  }
+
   render() {
+    const { todoes } = this.props;
     return (
       <div class="container">
         <div class="text-center">
           <h1 class="mt-3">Redux Test</h1>
+          <hr />
+          <form onSubmit={this.onSubmitTodo} ref={elem => this.elements = elem && elem.elements}>
+            <input type="text" name="todo" placeholder="TODOを入力" /><br />
+            <input type="submit" value="追加" />
+          </form>
+          <div class="mt-3">
+            <table class="table table-stripe">
+              <thead>
+                <tr><th>Index</th><th>TODO</th><th>Done</th></tr>
+              </thead>
+              <tbody>
+                {todoes.map((todo, index) => {
+                  return (
+                    <tr>
+                      <td>index</td>
+                      <td>todo.text</td>
+                      <td><input type="checkbox" /> </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     );
